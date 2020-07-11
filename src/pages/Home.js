cd0538ee265ce7e7492f1113eb5a5d4cc701adac
Arthur Kay <arthurakay@gmail.com>
2020-07-11 13:52:59 -0500

    Starting to customize the app, and build yaml

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Home = () => (
+    <div>
+        <h1>Cyber Scavenger Hunt</h1>
+
+        <p>
+            Welcome to step one of this 10-step scavenger hunt!
+        </p>
+
+        <p>
+            Reconnassaince against a web application begins with the text on the website itself.
+            The better you understand the company, and who the developers behind the code are...
+            the better the conclusions you can infer.
+        </p>
+
+        <p>
+            Hint #1: If I were a search engine, I'd look here to make sure I don't index any content I'm not supposed to.
+        </p>
+
+        <p>
+            Hint #2: Robots...
+        </p>
+
+        <p>
+            Hint #3: Most websites have a <a href="/robots.txt">robots.txt</a> file. Take a look to see what's in there!
+        </p>
+    </div>
+);
+
+export default Home;
