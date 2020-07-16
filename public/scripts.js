96278c2caafa0de7b60dac472f4fc39d45d2a41a
Arthur Kay <arthurakay@gmail.com>
2020-07-15 20:22:34 -0500

    All steps added except for #6 (HTTP Headers)

---
+++ public/scripts.js
@@ -0,0 +1,19 @@
+window.localStorage.setItem('egg7', `
+    CONGRATULATIONS! You found the 7th of 8 easter eggs in our scavenger hunt!
+
+    LocalStorage is one of a number of places a developer might choose to store 
+    some application data. These values are normally injected somewhere in the 
+    user interface or used in the business logic for the application.
+
+    An attacker might try to compromise these values...
+
+    What about cookies? Developers also put lots of stuff in your cookies, and 
+    because there isn't much space allowed in a cookie it's often encoded, 
+    encrypted or hashed.
+
+    Hint #1: Look at the cookies in DevTools, specifically one named "session".
+
+    Hint #2: Do a Google search to "Base64 Decode" the value you find in the cookie.
+`);
+
+document.cookie = "session=Q09OR1JBVFVMQVRJT05TISBZb3UgZm91bmQgYWxsIDggZWFzdGVyIGVnZ3MgaW4gb3VyIHNjYXZlbmdlciBodW50IQoKVXN1YWxseSBzZXNzaW9ucyBhcmVu4oCZdCBCYXNlNjQgRW5jb2RlZCwgYnV0IGxvdHMgb2Ygb3RoZXIgZGF0YSBpcy4gSWYgaXQncyAKdGhpcyBlYXN5IHRvIGhpZGUgZGF0YSBpbiBwbGFpbiBzaWdodCwgZG8geW91IGhhdmUgYSBuZXcgcGVyc3BlY3RpdmUgb24gY29va2llIApsYXdzIGFuZCBkYXRhIHByaXZhY3k/CgpFbWFpbCBtZSAoYXJ0QGFrYXdlYmRlc2lnbi5jb20pIHdoYXQgeW91IGZpbmQgYXQgdGhlIFVSTCBiZWxvdyBhbmQgdGVsbCBtZSAKaG93IHlvdSBsaWtlZCB0aGlzIEN5YmVyIFNjYXZlbmdlciBIdW50IQoKICAqIGh0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9b0hnNVNKWVJIQTA=";
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,46 +1,99 @@
 import React from 'react';
 
 console.log(`
-    CONGRATULATIONS! You found the 3rd of 10 easter eggs in our scavenger hunt!
+    CONGRATULATIONS! You found the 3rd of 8 easter eggs in our scavenger hunt!
 
-    Web developers are pretty lazy sometimes, and we sometimes forget to take out messages that help us debug our code.
+    Web developers are pretty lazy sometimes, and we sometimes forget to take out 
+    messages that help us debug our code.
 
-    What else can you see in these nifty DevTools?
-
-    Hint #1: Look in the JS file for another hidden page.
-
-    Hint #2: Still can't find it? 
+    For example...
 `);
 
-const Admin = () => (
-    <div>
-        <h1>Admin Page?</h1>
+class Admin extends React.Component {
+    state = {
+        authenticated: false
+    }
 
-        <p>
-            CONGRATULATIONS! You found the 2nd of 10 easter eggs in our scavenger hunt!
-        </p>
+    componentDidMount() {
+        const url = new URL(window.location);
 
-        <p>
-            It's a good thing this website doesn't actually have an open administrator's dashboard!
-        </p>
+        if (!url.searchParams.has('authenticated')) {
+            window.location = '/admin?authenticated=false';
+        } else if (url.searchParams.get('authenticated') === 'true') {
+            this.setState({
+                authenticated: true
+            });
+        } else {
+            console.log('What happens if we change the URL parameter "authenticated" to true...');
+        }
+    }
 
-        <p>
-            The developer who wrote this app definitely didn't think anyone would look here,
-            but that's why robots.txt can be such a problem for security.
-        </p>
+    easterEgg2() {
+        return (
+            <div>
+                <h1>Admin Page?</h1>
 
-        <p>
-            Next thing I'd probably do is open my browser's DevTools (show image?) and see what I can find...
-        </p>
+                <p>
+                    CONGRATULATIONS! You found the 2nd of 8 easter eggs in our scavenger hunt!
+                </p>
 
-        <p>
-            Hint #1: How to open DevTools in the browser...
-        </p>
+                <p>
+                    It's a good thing this website doesn't actually have an open administrator's dashboard!
+                </p>
 
-        <p>
-            Hint #2: JS Console!
-        </p>
-    </div>
-);
+                <p>
+                    The developer who wrote this app definitely didn't think anyone would look here,
+                    but that's why robots.txt can be such a problem for security.
+                </p>
+
+                <p>
+                    Next thing I'd probably do is open my browser's DevTools (show image?) and see what I can find...
+                </p>
+
+                <p>
+                    Hint #1: How to open DevTools in the browser...
+                </p>
+
+                <p>
+                    Hint #2: JS Console!
+                </p>
+            </div>
+        );
+    }
+
+    easterEgg4() {
+        return (
+            <div>
+                <h1>Admin Page!!!</h1>
+
+                <p>
+                    CONGRATULATIONS! You found the 4th of 8 easter eggs in our scavenger hunt!
+                </p>
+
+                <p>
+                    The query string params...
+                </p>
+
+                <p>
+                    Also known as priviledge escalation...
+                </p>
+
+                <p>
+                    From here you'll be able to see <a href="/hidden.html">other pages</a> in the application you hadn't known about.
+                </p>
+            </div>
+        );
+    }
+
+    render() {
+        const { authenticated } = this.state;
+
+        return (
+            <>
+                {authenticated ? this.easterEgg4() : this.easterEgg2()}
+            </>
+        );
+    }
+}
 
 export default Admin;
